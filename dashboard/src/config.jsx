aef69e660f3c6872209a2c8101130ff5d5130ff5
Hasanweb <hasanboyjumanov4@gmail>
2023-07-22 15:46:53 +0500

    go

--- dashboard/src/config.jsx
+++ dashboard/src/config.jsx
@@ -1,7 +1,4 @@
 export const API_LINK = 'http://localhost:5000/api'
-<<<<<<< HEAD
-=======
-// export const API_LINK = 'http://192.168.1.4:5000/api'
->>>>>>> ee5d96d311c0fe4953345b688a08da054098d20a
+
 
 
